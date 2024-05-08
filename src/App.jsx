7d59af384474ead932b32e9f0ff9647bf9e691c4
Ankit Malik <68542020+ankitmalik84@users.noreply.github.com>
2024-05-08 21:04:33 +0530

    modified:   package.json

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ import {
   Timer,
   TimelineSection,
 } from "./components";
-import AnimatedCursor from "react-animated-cursor";
 import AOS from "aos";
 
 import "aos/dist/aos.css";
@@ -26,27 +25,6 @@ const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
       <Router basename="/">
-        <AnimatedCursor
-          innerSize={15}
-          outerSize={20}
-          color="125, 231, 235"
-          outerAlpha={0.2}
-          innerScale={0.7}
-          outerScale={5}
-          clickables={[
-            "a",
-            'input[type="text"]',
-            'input[type="email"]',
-            'input[type="number"]',
-            'input[type="submit"]',
-            'input[type="image"]',
-            "label[for]",
-            "select",
-            "textarea",
-            "button",
-            ".link",
-          ]}
-        />
         <div className={`${styles.paddingX} ${styles.flexCenter}`}>
           <div className={`${styles.boxWidth}`}>
             <Navbar />
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -26,7 +26,7 @@ const Services = () => {
         data-aos-duration="500"
         data-aos-easing="ease-in-out"
       >
-        Build Your Dream
+        Your journey of Revenue cycles begins here
       </h1>
       <h1
         className="flex-1 font-poppins font-semibold ss:text-[45px] text-[32px] text-white ss:leading-[70.8px] leading-[75px] text-center sm:text-left flex items-center justify-center p-5"
@@ -52,13 +52,13 @@ const Services = () => {
                 data-aos-easing="ease-in-out"
               >
                 <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-                  <img
+                  {/* <img
                     alt="gallery"
                     className="block h-68 w-full object-cover object-center"
                     src={images[index]} // Change to images[`img${index + 1}`]
-                  />
+                  /> */}
                   <div className="p-6">
-                    <h1 className="text-xl font-semibold text-gray-800 mb-2 ss:text-[24px] text-[32px] text-center">
+                    <h1 className="text-xl font-bold text-[#49CEDB]  mb-2 ss:text-[26px] text-[34px] text-center">
                       {content[key][0]} {/* Access content using key */}
                     </h1>
                     <p className="text-gray-700 text-base text-center">
