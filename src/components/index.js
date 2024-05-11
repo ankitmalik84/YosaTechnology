d2ad0f4990ec3f0ba9e718eccaaad18be43654ee
Ankit Malik <68542020+ankitmalik84@users.noreply.github.com>
2024-05-11 12:45:47 +0530

    modified:   src/components/index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,27 +1,10 @@
 import Navbar from "./Navbar";
 import AboutUs from "./AboutUs";
-import Participat from "./Participat";
-import Sponser from "./Sponser";
 import Footer from "./Footer";
 import Hero from "./Hero";
-import Team from "./Team";
 import Mentor from "./Mentor";
 import FAQ from "./FAQ";
 import Timer from "./Timer";
 import Services from "./Services";
-import TimelineSection from "./TimelineSection";
 
-export {
-  Navbar,
-  AboutUs,
-  Participat,
-  Sponser,
-  Services,
-  Footer,
-  Hero,
-  Team,
-  Mentor,
-  FAQ,
-  Timer,
-  TimelineSection,
-};
+export { Navbar, AboutUs, Services, Footer, Hero, Mentor, FAQ, Timer };
